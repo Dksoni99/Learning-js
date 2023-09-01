d315279ca351ea9383e998dd16bb2b62f47f73a2
Dksoni99 <uiuxdesigner99@gmail.com>
2023-09-01 06:48:43 +0000

    conversion-operation done

---
+++ 01_basics/conversion-operation.js
@@ -0,0 +1,15 @@
+let score = 54;
+
+// console.log(typeof(score));
+
+let score1 = "55";
+
+console.log(typeof score1)
+
+let booleanName = 0;
+
+console.log(Boolean(booleanName));
+
+let myNume = undefined;
+
+console.log(typeof myNume)
