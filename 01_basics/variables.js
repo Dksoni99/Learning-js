00fc30138934db1cc11b3a019f76e520a70d1de4
Dksoni99 <uiuxdesigner99@gmail.com>
2023-09-01 04:03:08 +0000

    variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const empID = "12345D";
+let empName = "Deepak kumar";
+var CompanyName = "HCL Technology pvt. ltd.";
+emailID = "deepak.kumar@gmail.com";
+
+
+console.table([empID,empName,CompanyName,empID]);
